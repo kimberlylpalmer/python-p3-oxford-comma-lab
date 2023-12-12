22313b6fe9af73cb246d0c5a906bc5eace9f3461
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-12-12 01:09:42 -0800

    lab complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    num_items = len(items)
+    if num_items == 0:
+        return ""
+    elif num_items == 1:
+        return items[0]
+    elif num_items == 2:
+        return " and ".join(items)
+    else:
+        return ", ".join(items[:-1]) + ", and " + items[-1]
